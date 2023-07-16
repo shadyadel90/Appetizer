a3d88dd06cae3a958f22c10bab5991ef399a5513
shadyadel90 <31343078+shadyadel90@users.noreply.github.com>
2023-07-16 11:57:49 +0300

    updated ui tests

--- Appetizer/AppetizerUITests/AppetizerUITests.swift
+++ Appetizer/AppetizerUITests/AppetizerUITests.swift
@@ -8,16 +8,16 @@
 import XCTest
 
 final class AppetizerUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         continueAfterFailure = false
-
+        
     }
-
+    
     override func tearDownWithError() throws {
         
     }
-
+    
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -25,157 +25,162 @@ final class AppetizerUITests: XCTestCase {
         
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    func testwithInvalidPassword() throws {
+    func testLoginWithWrongPassword() throws {
         
         let app = XCUIApplication()
         app.launch()
         
+        // Tap on "Login" button on the navigation bar
         app.navigationBars["Choose your Appetizer"].buttons["Login"].tap()
-        app.textFields["Username"].typeText("shadyadel")
         
-        //test password textfield
+        // Enter the username
+        let usernameTextField = app.textFields["Username"]
+        XCTAssertTrue(usernameTextField.exists)
+        XCTAssertTrue(usernameTextField.isEnabled)
+        usernameTextField.tap()
+        usernameTextField.typeText("shadyadel")
+        
+        // Enter the password
         let passwordTextField = app.secureTextFields["Password"]
+        XCTAssertTrue(passwordTextField.exists)
+        XCTAssertTrue(passwordTextField.isEnabled)
         passwordTextField.tap()
-        XCTAssertTrue(passwordTextField.exists)
-        XCTAssertTrue(passwordTextField.isEnabled)
-
-        app.buttons["Log in"].tap()
-        
-        // test wrong password alert
-        let passwordSecureTextField = app.secureTextFields["Password"]
-        XCTAssertTrue(passwordTextField.exists)
-        XCTAssertTrue(passwordTextField.isEnabled)
-        passwordSecureTextField.tap()
         passwordTextField.typeText("1111")
-        app/*@START_MENU_TOKEN@*/.staticTexts["Log in"]/*[[".buttons[\"Log in\"].staticTexts[\"Log in\"]",".staticTexts[\"Log in\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
-        let passwordInvalidAlert = app.alerts["Oops"].scrollViews.otherElements
-        XCTAssertTrue(passwordInvalidAlert.element.exists)
-        passwordInvalidAlert.staticTexts["Oops"].tap()
+        // Tap on "Log in" button
+        let loginButton = app.buttons["Log in"]
+        XCTAssertTrue(loginButton.exists)
+        loginButton.tap()
+        
+        // Verify the alert for wrong password
+        let passwordInvalidAlert = app.alerts["Oops"]
+        XCTAssertTrue(passwordInvalidAlert.exists)
+        XCTAssertEqual(passwordInvalidAlert.staticTexts["Oops"].label, "Oops")
+        
+        // Tap on "OK" button to dismiss the alert
         passwordInvalidAlert.buttons["OK"].tap()
-    
         app.terminate()
     }
     
-    func testwithInvalidUsername() throws {
+    func testLoginWithInvalidUsername() throws {
         let app = XCUIApplication()
         app.launch()
-        
+        // Tap on "Login" button on the navigation bar
         app.navigationBars["Choose your Appetizer"].buttons["Login"].tap()
         
+        // Find the username text field and tap on it
         let usernameTextField = app.textFields["Username"]
         usernameTextField.tap()
         
-        let logInStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Log in"]/*[[".buttons[\"Log in\"].staticTexts[\"Log in\"]",".staticTexts[\"Log in\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        // Find the "Log in" static text and tap on it
+        let logInStaticText = app.staticTexts["Log in"]
         logInStaticText.tap()
         
-        // test empty textfield alert
+        // Test empty text field alert
         let elementsQuery = app.alerts["Oops"].scrollViews.otherElements
         let blankAlert = elementsQuery.staticTexts["We can't proceed because one of the fields is blank. Please note that all fields are required."]
         XCTAssertTrue(blankAlert.exists)
         blankAlert.tap()
         
+        // Find the "OK" button on the alert and tap on it
         let okButton = elementsQuery.buttons["OK"]
         XCTAssertTrue(okButton.exists)
         XCTAssertTrue(okButton.isEnabled)
         okButton.tap()
         
+        // Clear the username text field, enter "qwer", and tap on "Log in" static text
         usernameTextField.tap()
         usernameTextField.typeText("qwer")
         logInStaticText.tap()
-        okButton.tap()
         
-        // test wrong username alert
-        app.secureTextFields["Password"].tap()
-        app.secureTextFields["Password"].typeText("1111")
+        // Test wrong username alert
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("1111")
         app.buttons["Log in"].tap()
         
-        let usernameInvalidalert = elementsQuery.staticTexts["Username is incorrect"]
-        XCTAssertTrue(usernameInvalidalert.exists)
+        let usernameInvalidAlert = elementsQuery.staticTexts["Username is incorrect"]
+        XCTAssertTrue(usernameInvalidAlert.exists)
         okButton.tap()
         
         app.terminate()
         
     }
     
-    func testWithRightCredential() throws{
+    func testLoginWithValidCredentials() throws {
         let app = XCUIApplication()
         app.launch()
         
-        //choose cell
-        let cell = app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"Crispy Fish Goujons ")/*[[".cells.containing(.staticText, identifier:\"Crispy Fish Goujons \")",".cells.containing(.staticText, identifier:\"516 kcal\")"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.children(matching: .other).element(boundBy: 2)
-        cell.waitForExistence(timeout: 2)
-        XCTAssertTrue(cell.exists)
+        // Choose cell
+        let cell = app.tables.cells.containing(.staticText, identifier: "Crispy Fish Goujons ").children(matching: .other).element(boundBy: 2)
+        XCTAssertTrue(cell.waitForExistence(timeout: 2), "Cell exists")
         cell.tap()
         
-        //test seque alert
-        let elementsQuery = app.alerts["Oops"].scrollViews.otherElements
-        let firstalert = elementsQuery.staticTexts["We can't proceed to details as you didn't log in"]
-        firstalert.waitForExistence(timeout: 2)
-        firstalert.tap()
-        XCTAssertTrue(firstalert.exists)
-        let alertButton =  elementsQuery.buttons["OK"]
-        XCTAssertTrue(alertButton.exists)
-        alertButton.tap()
+        // Test segue alert
+        let alert = app.alerts.element
+        let alertText = alert.staticTexts["We can't proceed to details as you didn't log in"]
+        XCTAssertTrue(alertText.exists, "Alert text exists")
+        alertText.tap()
         
-        // test login button
+        let okButton = alert.buttons["OK"]
+        XCTAssertTrue(okButton.exists, "OK button exists")
+        okButton.tap()
+        
+        // Test login button
         let navigationButton = app.navigationBars["Choose your Appetizer"].buttons["Login"]
-        XCTAssertTrue(navigationButton.exists)
-        XCTAssertTrue(navigationButton.isEnabled)
-        app.navigationBars["Choose your Appetizer"].buttons["Login"].tap()
+        XCTAssertTrue(navigationButton.exists, "Login button exists")
+        XCTAssertTrue(navigationButton.isEnabled, "Login button is enabled")
+        navigationButton.tap()
         
-        // test username textfield
+        // Test username textfield
         let usernameTextField = app.textFields["Username"]
-        XCTAssertTrue(usernameTextField.exists)
-        app.textFields["Username"].tap()
-        XCTAssertTrue(usernameTextField.isEnabled)
-        app.textFields["Username"].typeText("shadyadel")
+        XCTAssertTrue(usernameTextField.exists, "Username textfield exists")
+        XCTAssertTrue(usernameTextField.isEnabled, "Username textfield is enabled")
+        usernameTextField.tap()
+        usernameTextField.typeText("shadyadel")
         
-        //test password textfield
+        // Test password textfield
         let passwordSecureTextField = app.secureTextFields["Password"]
-        XCTAssertTrue(passwordSecureTextField.exists)
+        XCTAssertTrue(passwordSecureTextField.exists, "Password textfield exists")
+        XCTAssertTrue(passwordSecureTextField.isEnabled, "Password textfield is enabled")
         passwordSecureTextField.tap()
-        XCTAssertTrue(passwordSecureTextField.isEnabled)
         passwordSecureTextField.typeText("1234")
         
-        // test login button validator
-       let loginButton =  app/*@START_MENU_TOKEN@*/.buttons["Log in"].staticTexts["Log in"]/*[[".buttons[\"Log in\"].staticTexts[\"Log in\"]",".staticTexts[\"Log in\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        XCTAssertTrue(loginButton.isEnabled)
-        XCTAssertTrue(loginButton.exists)
+        // Test login button validator
+        let loginButton = app.buttons["Log in"]
+        XCTAssertTrue(loginButton.isEnabled, "Login button is enabled")
+        XCTAssertTrue(loginButton.exists, "Login button exists")
         loginButton.tap()
         
-        //test segue after unlock
+        // Test segue after unlock
         let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"516 kcal")/*[[".cells.containing(.staticText, identifier:\"Crispy Fish Goujons \")",".cells.containing(.staticText, identifier:\"516 kcal\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.children(matching: .other).element(boundBy: 2).tap()
+        let recipeCell = tablesQuery.staticTexts["516 kcal"]
+        XCTAssertTrue(recipeCell.exists, "Recipe cell exists")
+        recipeCell.tap()
         
-        //test exist of first cell
-        let firstcell = tablesQuery.staticTexts["516 kcal"]
-        XCTAssertTrue(firstcell.exists)
-        firstcell.swipeUp(velocity: 50)
-        firstcell.swipeDown(velocity: 50)
-        
-        //test favourite button in detail view
-        let heartButton = tablesQuery.buttons["love"]
-        XCTAssertTrue(heartButton.exists)
-        XCTAssertTrue(heartButton.isEnabled)
+        // Test favorite button in detail view
+        let heartButton = app.buttons["love"]
+        XCTAssertTrue(heartButton.exists, "Favorite button exists")
+        XCTAssertTrue(heartButton.isEnabled, "Favorite button is enabled")
         heartButton.tap()
         
-        // test back button
+        // Test back button
         let backButton = app.navigationBars["Appetizer.RecipeDetailView"].buttons["Choose your Appetizer"]
-        XCTAssertTrue(backButton.exists)
-        XCTAssertTrue(backButton.isEnabled)
+        XCTAssertTrue(backButton.exists, "Back button exists")
+        XCTAssertTrue(backButton.isEnabled, "Back button is enabled")
         backButton.tap()
         
-        //test favorite mark on recipe list
-        app.waitForExistence(timeout: 1)
-        let element = tablesQuery/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"516 kcal")/*[[".cells.containing(.staticText, identifier:\"Crispy Fish Goujons \")",".cells.containing(.staticText, identifier:\"516 kcal\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.children(matching: .other).element(boundBy: 2)
-        element.tap()
-        tablesQuery.buttons["love"].tap()
-        app.navigationBars["Appetizer.RecipeDetailView"].buttons["Choose your Appetizer"].tap()
-        app.waitForExistence(timeout: 2)
+        // Test favorite mark on recipe list
+        let tableCell = app.tables.staticTexts["Crispy Fish Goujons "]
+        XCTAssertTrue(tableCell.exists, "Recipe cell exists")
+        tableCell.tap()
+        heartButton.tap()
+        backButton.tap()
+        
+        
         app.terminate()
-                                
     }
+    
     
     
     func testLaunchPerformance() throws {
